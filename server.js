7d205893037c1246ff5efaae96d9a28a81248f6d
jschireson <64047275+jschireson@users.noreply.github.com>
2022-06-13 12:22:00 -0400

    Express start

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
-function print(){
-    console.log("hello")
+const express = require('express')
+const app = express()
+// const session = require('express-session');
 
-}
 
-print();
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: true,
+//     secret: 'SECRET' 
+//   }));
+  
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+    // res.render('login')
+    res.render('index')
+})
+
+console.log("Starting up at localhost:3000");
+app.listen(3000);
+
+// function print(){
+//     console.log("hello")
+
+// }
+
+// print();
